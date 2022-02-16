d815d24ed13b00117dfb600c627348992d67e5e7
Ezabulbary <ezabul.bary@gmail.com>
2022-02-16 16:59:19 +0600

    factorial

---
+++ factorial.js
@@ -0,0 +1,18 @@
+let factorial = 1;
+
+for(let i = 4; i >= 1; i--){
+    factorial = factorial * i;
+}
+
+console.log(factorial);
+
+// factorial 
+
+function factorial(i){
+    if(i == 1){
+        return 1;
+    }
+    return i * factorial(--i);
+}
+
+console.log(factorial(5));
